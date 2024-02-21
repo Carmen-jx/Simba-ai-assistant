ff2e14f3f5754b5eb48e6d372db7d05056f0d69c
clam98 <clam98@myseneca.ca>
2024-02-20 23:19:39 -0500

    added functionality to recite poems

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ import pyttsx3
 import webbrowser
 import wikipedia
 import wolframalpha
+import requests
+import random
 
 # speech engine initialization
 
@@ -19,6 +21,8 @@ activateWord = 'simba'
 
 #configure wolfram alpha
 app_id = 'UXRVW7-R3P3A66PV7'
+wolf_client = wolframalpha.Client('UXRVW7-R3P3A66PV7')
+
 
 def speak(text, rate=180):
     engine.setProperty('rate', rate)
@@ -39,7 +43,7 @@ def parse_command():
     try:
         print('Recognizing speech...')
         query = listener.recognize_google(input_speech, language='en_GB')
-        print('The input speech was: {query')
+        print('The input speech was:', query)
     except Exception as exception:
         print('I did not catch that')
         speak('I did not catch that')
@@ -61,6 +65,52 @@ def search_wiki(query= ' '):
     print(wikiPage.title)
     wikiSummary = str(wikiPage.summary)
     return wikiSummary
+
+def wolf_type(var):
+    if isinstance(var, list):
+        return var[0]['plaintext']
+    else:
+        return var['plaintext']
+
+def search_wolframalpa(query = ' '):
+    response = wolf_client.query(query)
+
+    if response['@success'] == 'false':
+        return 'Could not compute'
+    else:
+        result = ''
+        #question
+        pod0 = response['pod'][0]
+
+        pod1 = response['pod'][1]
+
+        if ('result' in pod1['@title'].lower()) or (pod1.get('@primary', 'false') == 'true') or ('definition' in pod1['@title'].lower()):
+            result = wolf_type(pod1['subpod'])
+
+            return result.split('(')[0]
+        else:
+            question = wolf_type(pod0['subpod'])
+
+            return result.split('(')[0]
+            speak('computation failed. searching wikipedia database')
+            return search_wiki(question)
+
+
+
+def fetch_poem():
+    # configure poem API request
+    response = requests.get("https://www.beanpoems.com/api/poems")
+    print(response.json())
+
+    if response.status_code == 200:
+        return response.json()
+    else:
+        print("failed to fetch poem:", response.status_code)
+        return None
+
+
+
+
 
 if __name__ == '__main__':
     speak('hi what can I help you with')
@@ -84,7 +134,7 @@ if __name__ == '__main__':
                 query = ' '.join(query[2:])
                 print(query)
                 try:
-                    url= 'http://' +query
+                    url = 'http://' + query
                     webbrowser.get('chrome').open_new(url)
                     print("Browser opened successfully.")  # Debugging
                 except Exception as e:
@@ -98,6 +148,39 @@ if __name__ == '__main__':
                     speak(search_wiki(query))
                 except Exception as e:
                     speak(query + 'cannot be found, try again')
+
+            if query[0] == 'compute':
+                query = ' '.join((query[1:]))
+                speak('computing')
+                try:
+                    result = search_wolframalpa(query)
+                    speak(result)
+                except Exception as e:
+                    speak('unable to compute')
+                    print(e)
+
+            if query[0] == "poem":
+                try:
+                    poems_list = fetch_poem()
+
+                    poem = poems_list[random.randint(0,5)]
+
+                    if poem:
+                        title = poem.get('title')
+                        description = poem.get('description')
+                        body = poem.get('body')
+
+                        speak('Title: ' + title, 150)
+                        speak(description, 150)
+                        speak(body, 150)
+                    else:
+                        speak('Cannot find a poem')
+                except Exception as e:
+                    speak('unable to read a poem')
+                    print(e)
+
+
+
 
 
 
